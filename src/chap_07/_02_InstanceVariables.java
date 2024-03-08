c24e1fbb3e0002ec54ade711e2a1c2d888d113ac
seeyoungryu <seeyoungryu@gmail.com>
2024-03-08 19:12:18 +0900

    add: 클래스-인스턴스 개념

--- src/chap_07/_02_InstanceVariables.java
+++ src/chap_07/_02_InstanceVariables.java
@@ -3,8 +3,8 @@ package chap_07;
 public class _02_InstanceVariables {
     public static void main(String[] args) {
         // 처음 만든 블랙박스
-        BlackBox b1 = new BlackBox();
-        b1.modelName = "까망이";
+        BlackBox b1 = new BlackBox();        //new 이용해서 블랙박스(클래스) 에 대한 객체(인스턴스) 생성
+        b1.modelName = "까망이";            //만들어낸 객체에-클래스-쓸수있는 필드 . 으로 사용
         b1.resolution = "FHD";
         b1.price = 200000;
         b1.color = "블랙";
@@ -15,6 +15,7 @@ public class _02_InstanceVariables {
         System.out.println(b1.color);
 
         System.out.println("-------------------");
+
 
         // 새로운 블랙박스 제품
         BlackBox b2 = new BlackBox();
@@ -29,3 +30,6 @@ public class _02_InstanceVariables {
         System.out.println(b2.color);
     }
 }
+
+
+// ------------> 만들어낸 두개의 객체만으로 데이터 다 표현함
