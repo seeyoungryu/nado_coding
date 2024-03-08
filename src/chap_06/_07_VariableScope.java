eb072af9f2a9877d910178436a2ea6284f0dc260
seeyoungryu <seeyoungryu@gmail.com>
2024-03-08 17:40:28 +0900

    update: method

--- src/chap_06/_07_VariableScope.java
+++ src/chap_06/_07_VariableScope.java
@@ -1,14 +1,17 @@
 package chap_06;
 
+
 public class _07_VariableScope {
     public static void methodA(int number) {
-        //System.out.println(number);
-        //System.out.println(result);
     }
+
+    //메소드를 호출하는 쪽에서 매개변수의 이름은 다를 수 있음
 
     public static void methodB() {
         int result = 1; // 지역 변수
     }
+    //지역변수-> 그 변수가 선언된 영역 - 중괄호 내에서만 사용할 수 있음   >> 전달값으로 사용?
+
 
     public static void main(String[] args) {
         int number = 3;
